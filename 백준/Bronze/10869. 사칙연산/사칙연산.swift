ee80eb9111fd1e32ab1f89d0d123745c19526e46
yuvinrho <yvrho@naver.com>
2022-06-15 20:55:29 +0900

    [Bronze V] Title: 사칙연산, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.swift
@@ -0,0 +1,6 @@
+let nums = readLine()!.split(separator: " ").map{ Int($0)! }
+print( nums[0] + nums[1] )
+print( nums[0] - nums[1] )
+print( nums[0] * nums[1] )
+print( nums[0] / nums[1] )
+print( nums[0] % nums[1] )
