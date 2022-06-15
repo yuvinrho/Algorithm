e5ed56d21fead8a3a30025bc2886dd3dfed5b848
yuvinrho <yvrho@naver.com>
2022-06-15 20:51:44 +0900

    [Bronze V] Title: 사분면 고르기, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.swift
@@ -0,0 +1,25 @@
+let x = Int(readLine()!)!
+let y = Int(readLine()!)!
+
+if x > 0 {
+    if y > 0 {
+        print("1")
+    }
+    else if y < 0 {
+        print("4")
+    }
+    else {
+        
+    }
+}
+else {
+    if y > 0 {
+        print("2")
+    }
+    else if y < 0 {
+        print("3")
+    }
+    else {
+        
+    }
+}
