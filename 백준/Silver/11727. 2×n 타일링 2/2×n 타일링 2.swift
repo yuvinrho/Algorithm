85c6b466ba4475aeb96b6af7f1d3f3468d116250
yuvinrho <yvrho@naver.com>
2022-06-15 20:51:58 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 16 ms, Memory: 79508 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+let n = Int(readLine()!)!
+var dp = Array.init(repeating: 0, count: n + 1)
+
+for i in 1...n {
+    if i == 1 { dp[i] = 1 }
+    if i == 2 { dp[i] = 3 }
+    
+    if i > 2 {
+        dp[i] = (dp[i - 1] + dp[i - 2] * 2) % 10007
+    }
+}
+
+print(dp[n])
