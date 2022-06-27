d0535e9721216db15c35eea8eb599f3960674f1e
yuvinrho <yvrho@naver.com>
2022-06-27 22:59:58 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 16 ms, Memory: 79512 KB -BaekjoonHub

---
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+let input = readLine()!.split(separator: " ").map { Int($0)! }
+let (x, y, w, h) = (input[0], input[1], input[2], input[3])
+let distances = [x - 0, y - 0, w - x, h - y]
+print(distances.min()!)
