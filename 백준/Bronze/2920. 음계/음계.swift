69fc8cc7ba15de476b774a66ce1cc4814abffd81
yuvinrho <yvrho@naver.com>
2022-06-27 22:39:06 +0900

    [Bronze II] Title: 음계, Time: 12 ms, Memory: 79504 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.swift
+++ 백준/Bronze/2920. 음계/음계.swift
@@ -1,33 +1,4 @@
 import Foundation
 
-let numbers = readLine()!.split(separator: " ").map { Int($0)! }
-
-func isAscending(_ numbers:[Int]) -> Bool {
-    var prevNumber = numbers[0]
-    for i in 1..<numbers.count {
-        if prevNumber == numbers[i] - 1 {
-            prevNumber = numbers[i]
-            continue
-        } else {
-            return false
-        }
-    }
-    return true
-}
-
-func isDescending(_ numbers:[Int]) -> Bool {
-    var prevNumber = numbers[0]
-    for i in 1..<numbers.count {
-        if prevNumber == numbers[i] + 1 {
-            prevNumber = numbers[i]
-            continue
-        } else {
-            return false
-        }
-    }
-    return true
-}
-
-if isAscending(numbers) { print("ascending"); exit(0) }
-if isDescending(numbers) { print("descending"); exit(0) }
-print("mixed")
+let numbers = readLine()!
+numbers == "1 2 3 4 5 6 7 8" ? print("ascending") : numbers == "8 7 6 5 4 3 2 1" ? print("descending") : print("mixed")
